42ee4b0ba7498cb328545a921e7f09fe5f1bca64
Neo21 <79481577+Neo2108@users.noreply.github.com>
2026-03-18 13:25:45 -0400

    [Dialog] - add character and context enums

---
+++ Assets/Scripts/Dialog/DialogEnums.cs
@@ -0,0 +1,21 @@
+public enum CharacterId
+{
+    Tennon,
+    Antuna,
+    Keanu,
+    Jose,
+    Amira,
+    Solomon
+}
+
+public enum DialogContext
+{
+    Intro,
+    ShopWelcome,
+    PuzzleHint,
+    StealthWarning,
+    RecruitmentNotice,
+    BossRiddle,
+    GenericChatter
+}
+
