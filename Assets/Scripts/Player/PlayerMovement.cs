25fd1586e503ef7d3c0ecafa4760ca22308d6c94
Nathan Chiu <blackbird6366@gmail.com>
2026-03-18 14:28:53 -0400

    Add attacking

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
+    private bool isAttacking = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,7 +21,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!isAttacking)
+        {
         rb.linearVelocity = moveInput * moveSpeed;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -37,9 +42,27 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
-        
-        
-
-        
     }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack input received");
+        if (context.started) // when key is pressed down
+        {
+            isAttacking = true;
+            animator.SetBool("isAttacking", true);
+            if(moveInput != Vector2.zero)
+            {
+                animator.SetFloat("LastInputX", moveInput.x);
+                animator.SetFloat("LastInputY", moveInput.y);
+            }
+            rb.linearVelocity = Vector2.zero; // stop movement when attacking
+        }
+    }
+
+    public void EndAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("isAttacking", false);
+    }
+
 }
