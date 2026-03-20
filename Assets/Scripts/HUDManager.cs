0e1e32825f0a88d925289d9a69b90f96bf99700b
JBlack1010 <jblac49@uwo.ca>
2026-03-20 12:11:33 -0400

    Initital implementation of the HUDManager script (note: there are several methods left blank as placeholders as this class will have to interact gameManager + be attached to the text and btn's in Unity)

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -2,6 +2,86 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 
-public class HUDManager : MonoBehaviour {
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager Instance { get; private set; }
+
+    [Header("Health")]
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    [Header("Level & Boss")]
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI bossText;
+
+    [Header("Gold")]
+    [SerializeField] private TextMeshProUGUI goldText;
+
+    [Header("Character Switcher")]
+    [SerializeField] private TextMeshProUGUI characterNameText;
+
+    // Level and boss names (index 0-4)
+    private readonly string[] levelNames = { "Desert", "Jungle", "Ice", "Volcano", "City" };
+    private readonly string[] bossNames  =
+    {
+        "Antuna the Arms Dealer",
+        "Keanu the Ninja",
+        "Jose the Ice Mage",
+        "Amira the Lava Queen",
+        "Solomon the Ex-Boss"
+    };
     
+    private List<string> unlockedCharacters = new List<string> {"Tennon Lurco"};
+    private int currentCharacterIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        RefreshHUD();
+    }
+
+    private void Update()
+    {
+        RefreshHUD();
+    }
+
+    public void RefreshHUD() // this method will be used to update all info displayed in the HUD
+    {
+        
+    }
+    
+    // iterates to the next character in the list
+    public void OnNextCharacter()
+    {
+        if (unlockedCharacters.Count == 0) return;
+        currentCharacterIndex = (currentCharacterIndex + 1) % unlockedCharacters.Count;
+        UpdateCharacterDisplay();
+    }
+
+    // iterates to the prev character in the list
+    public void OnPrevCharacter()
+    {
+        if (unlockedCharacters.Count == 0) return;
+        currentCharacterIndex = (currentCharacterIndex - 1 + unlockedCharacters.Count) % unlockedCharacters.Count;
+        UpdateCharacterDisplay();
+    }
+
+    private void UpdateCharacterDisplay()
+    {
+        // will be used for fetching the character name (will need to change sprite to in other classes)
+    }
+
+    // called in different class (probably gameManager) to add a new unlocked character when a level is defeated
+    public void OnCharacterUnlocked(string characterName)
+    {
+        if (!unlockedCharacters.Contains(characterName))
+        {
+            unlockedCharacters.Add(characterName);
+            Debug.Log("[HUDManager] Character unlocked: " + characterName);
+        }
+    }
 }
