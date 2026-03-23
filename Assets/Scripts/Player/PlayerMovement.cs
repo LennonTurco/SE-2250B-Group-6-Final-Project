c8cd6f2e03dbde9474b98577ed28d9a7b14d2f37
Lennon Turco <lturco3@uwo.ca>
2026-03-23 14:00:47 -0400

    Fixing some sprite animations which got messed up via some metadata changes on prev merge

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,61 +1,54 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
     private bool isAttacking = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(!isAttacking)
+        if (!isAttacking)
         {
-        rb.linearVelocity = moveInput * moveSpeed;
+            rb.linearVelocity = moveInput * moveSpeed;
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        animator.SetBool("isWalking", true);
+        moveInput = context.ReadValue<Vector2>();
 
-        if (context.canceled)
+        if (moveInput != Vector2.zero)
         {
-            animator.SetBool("isWalking", false);
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
             animator.SetFloat("LastInputX", moveInput.x);
             animator.SetFloat("LastInputY", moveInput.y);
         }
-
-        moveInput = context.ReadValue<Vector2>();
-
-        animator.SetFloat("InputX", moveInput.x);
-        animator.SetFloat("InputY", moveInput.y);
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
+
     public void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Attack input received");
-        if (context.started) // when key is pressed down
+        if (context.started)
         {
+            Debug.Log("Attack input received");
             isAttacking = true;
             animator.SetBool("isAttacking", true);
-            if(moveInput != Vector2.zero)
-            {
-                animator.SetFloat("LastInputX", moveInput.x);
-                animator.SetFloat("LastInputY", moveInput.y);
-            }
-            rb.linearVelocity = Vector2.zero; // stop movement when attacking
+            rb.linearVelocity = Vector2.zero;
         }
     }
 
@@ -64,5 +57,4 @@ public class PlayerMovement : MonoBehaviour
         isAttacking = false;
         animator.SetBool("isAttacking", false);
     }
-
 }
