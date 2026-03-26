5c237d54e8079f972d83e6543cf000aa6f793715
Nathan Chiu <blackbird6366@gmail.com>
2026-03-25 23:50:30 -0400

    merge with neo-storyline-fix

--- Assets/Scripts/TestDialogInteractor.cs
+++ Assets/Scripts/TestDialogInteractor.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
 
-public class TestDialogInteractor : MonoBehaviour
-{
-    [SerializeField] private DialogTrigger target;
+// public class TestDialogInteractor : MonoBehaviour
+// {
+//     [SerializeField] private DialogTrigger target;
 
-    private void Update()
-    {
-        if (Keyboard.current != null && Keyboard.current.xKey.wasPressedThisFrame && target != null)
-        {
-            target.Interact();
-        }
-    }
-}
+//     private void Update()
+//     {
+//         if (Keyboard.current != null && Keyboard.current.xKey.wasPressedThisFrame && target != null)
+//         {
+//             target.Interact();
+//         }
+//     }
+// }
 
