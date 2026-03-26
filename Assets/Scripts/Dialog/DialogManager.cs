730211eba600f36ee1428bdba3828ad33ab05936
Lennon Turco <lturco3@uwo.ca>
2026-03-25 23:44:11 -0400

    Update DialogManager.cs

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -149,3 +149,4 @@ public class DialogManager : MonoBehaviour
         return isShowing;
     }
 }
+*/
