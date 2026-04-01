d4de80fb0763bbb7454578df409c0d21f5518758
Lennon Turco <lturco3@uwo.ca>
2026-04-01 01:56:52 -0400

    Fixed projectiles to break when hitting obstacles

--- Assets/Scripts/Enemies/BossProjectile.cs
+++ Assets/Scripts/Enemies/BossProjectile.cs
@@ -11,6 +11,13 @@ public class BossProjectile : MonoBehaviour
         {
             player.TakeDamage(damage);
             Destroy(gameObject);
+            return;
         }
+
+        // destroy on hitting anything that isn't the boss or another projectile
+        if (other.GetComponent<SolomonBoss>() != null) return;
+        if (other.GetComponent<BossProjectile>() != null) return;
+
+        Destroy(gameObject);
     }
 }
